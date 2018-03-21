0c317fb24dd65680c01509eed69c2863da75c943
Krishnakanth <mdskk3@gmail.com>
2018-03-21 14:07:42 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-
 var app = express();
 
 app.use(bodyParser.json());
@@ -26,3 +25,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+module.exports = {app};
